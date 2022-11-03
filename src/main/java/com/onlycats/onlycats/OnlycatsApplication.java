8f9b4555865c1e4d124b218d570b2abadc353dbe
Mikolaj Kornowicz <mikolaj.kornowicz@gmail.com>
2022-11-03 12:54:03 +0100

    first commit

---
+++ src/main/java/com/onlycats/onlycats/OnlycatsApplication.java
@@ -0,0 +1,13 @@
+package com.onlycats.onlycats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlycatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlycatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlycats/onlycats/OnlycatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlycats.onlycats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlycatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
